067a768e2713bd367ed3a4246e49dd5ac89b8232
Tony Sun <tony.sun427@gmail.com>
2019-07-24 20:56:34 -0700

    refactor jobs and worker code to use couch_jobs directly

--- src/search3_worker_manager.erl
+++ src/search3_worker_manager.erl
@@ -21,13 +21,13 @@ start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init(_) ->
+    process_flag(trap_exit, true),
     search3_jobs:set_timeout(),
-    State0 = #{
-        workers => #{},
-        acceptor_pid => undefined
+    State = #{
+        workers => [],
+        num_workers => num_workers()
     },
-    State = spawn_acceptor(State0),
-    {ok, State}.
+    {ok, spawn_workers(State)}.
 
 terminate(_, _St) ->
     ok.
@@ -35,58 +35,42 @@ terminate(_, _St) ->
 handle_call(Msg, _From, St) ->
     {stop, {bad_call, Msg}, {bad_call, Msg}, St}.
 
-handle_cast({job, Job, JobData}, State) ->
-    State1 = start_worker(State, Job, JobData),
-    State2 = spawn_acceptor(State1),
-    {noreply, State2};
 handle_cast(Msg, St) ->
     {stop, {bad_cast, Msg}, St}.
 
-handle_info({'DOWN', _Ref, process, Pid, Reason}, State) ->
-    LogMsg = "~p : process ~p exited with ~p",
-    couch_log:error(LogMsg, [?MODULE, Pid, Reason]),
-    State1 = check_finished_process(State, Pid),
-    {noreply, State1};
+handle_info({'EXIT', Pid, Reason}, State) ->
+    #{workers := Workers} = State,
+    case Workers -- [Pid] of
+        Workers ->
+            LogMsg = "~p : unknown process ~p exited with ~p",
+            couch_log:error(LogMsg, [?MODULE, Pid, Reason]),
+            {stop, {unknown_pid_exit, Pid}, State};
+        NewWorkers ->
+            if Reason == normal -> ok; true ->
+                LogMsg = "~p : indexer process ~p exited with ~p",
+                couch_log:error(LogMsg, [?MODULE, Pid, Reason])
+            end,
+            {noreply, spawn_workers(State#{workers := NewWorkers})}
+    end;
+
 handle_info(Msg, St) ->
-    couch_log:notice("~s ignoring info ~w", [?MODULE, Msg]),
-    {noreply, St}.
+    {stop, {bad_info, Msg}, St}.
 
 code_change(_OldVsn, St, _Extra) ->
     {ok, St}.
 
-start_worker(State, Job, JobData) ->
-    #{workers := Workers} = State,
-    {Pid, _Ref} = spawn_monitor(fun () -> search3_worker:start(Job, JobData) end),
-    Workers1 = Workers#{Pid => true},
-    State#{workers := Workers1}.
-
-spawn_acceptor(State) ->
+spawn_workers(State) ->
     #{
         workers := Workers,
-        acceptor_pid := Pid
+        num_workers := NumWorkers
     } = State,
-    MaxWorkers = config:get_integer("search3", "max_workers", ?MAX_WORKERS),
-    case maps:size(Workers) >= MaxWorkers of
-        false when not is_pid(Pid) ->
-            Parent = self(),
-            {Pid1, _Ref} = spawn_monitor(fun() -> blocking_acceptor(Parent) end),
-            State#{acceptor_pid := Pid1};
-        _ ->
+    case length(Workers) < NumWorkers of
+        true ->
+            Pid = search3_indexer:spawn_link(),
+            spawn_workers(State#{workers := [Pid | Workers]});
+        false ->
             State
     end.
 
-blocking_acceptor(Parent) ->
-    case search3_jobs:accept() of
-        not_found ->
-            blocking_acceptor(Parent);
-        {ok, Job, JobData} ->
-            gen_server:cast(Parent, {job, Job, JobData})
-    end.
-
-check_finished_process(#{acceptor_pid := Pid} = State, Pid) ->
-    State1 = State#{acceptor_pid := undefined},
-    spawn_acceptor(State1);
-check_finished_process(State, Pid) ->
-    #{workers := Workers} = State,
-    Workers1 = maps:remove(Pid, Workers),
-    State#{workers := Workers1}.
+num_workers() ->
+    config:get_integer("search3", "max_workers", ?MAX_WORKERS).
